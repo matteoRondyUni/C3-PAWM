0cf1b8ccc0c69c20ba8889bea5a216328edd10f3
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-04-01 15:46:37 +0200

    Aggiunto Alert come componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AlertFeatureNotImplementedComponent } from './alert-feature-not-implemented/alert-feature-not-implemented.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
+import { CookieAlertComponent } from './cookie-alert/cookie-alert.component';
 
 @NgModule({
-    declarations: [AlertFeatureNotImplementedComponent, PageFooterComponent],
-    exports: [AlertFeatureNotImplementedComponent, PageFooterComponent]
+    declarations: [AlertFeatureNotImplementedComponent, PageFooterComponent, CookieAlertComponent],
+    exports: [AlertFeatureNotImplementedComponent, PageFooterComponent, CookieAlertComponent]
 })
 export class ComponentsModule { }
