b0b768d3597ee1f74df3b6771eb9c1c6308090f7
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-04-06 15:31:55 +0200

    Update components.module.ts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { PageFooterComponent } from './page-footer/page-footer.component';
 import { CookieAlertComponent } from './cookie-alert/cookie-alert.component';
@@ -6,6 +8,7 @@ import { HeaderNavigationBarComponent } from './header-navigation-bar/header-nav
 
 @NgModule({
     declarations: [PageFooterComponent, CookieAlertComponent, HeaderNavigationBarComponent],
-    exports: [RouterModule, PageFooterComponent, CookieAlertComponent, HeaderNavigationBarComponent]
+    exports: [RouterModule, PageFooterComponent, CookieAlertComponent, HeaderNavigationBarComponent],
+    imports: [CommonModule, IonicModule]
 })
 export class ComponentsModule { }
