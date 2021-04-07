c9c825a120ce81d4e641def007c34d3c67a19c88
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-04-07 16:06:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const app = express();
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/www'));
 
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: __dirname + '/www' }
+    );
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-// ---- SERVE STATIC FILES ---- //
-//app.server.get('*.*', express.static(APPPATH, {maxAge: '1y'}));
-
-// ---- SERVE APLICATION PATHS ---- //
-app.all('*', function (req, res) {
-    res.status(200).sendFile(`/`, {root: APPPATH});
-});
