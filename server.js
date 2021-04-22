49329da5a380bda756f21659f55a91c9d6097b19
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2021-04-22 15:32:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./queries');
-const bodyParser = require('body-parser')
-// create application/json parser
-const jsonParser = bodyParser.json()
 
 //Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/www'));
 
-//app.get('/users', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-//})
+app.use(express.json());
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-app.post('/users', jsonParser, db.createUser)
-app.put('/users/:id', jsonParser, db.updateUser)
+app.post('/users', express.json(), db.createUser)
+app.put('/users/:id', express.json(), db.updateUser)
 app.delete('/users/:id', db.deleteUser)
-
-app.use(bodyParser());
 
 app.get('/*', function (req, res) {
     res.sendFile('index.html', { root: __dirname + '/www' }
