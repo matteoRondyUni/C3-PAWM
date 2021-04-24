74e215aec5f8f5f057c4e139e4699a76f0881fb9
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2021-04-24 19:46:17 +0200

    modifica pagina registrazione frontend

--- src/app/auth/registration/registration.page.ts
+++ src/app/auth/registration/registration.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from './../../services/authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
+  credentials: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      nome: ['', [Validators.required]],
+      cognome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      telefono: ['', [Validators.required]],
+      indirizzo: ['', [Validators.required]],
+      tipo: ['', [Validators.required]]
+    });
   }
 
   isRegistrationPage() {
@@ -17,5 +36,15 @@ export class RegistrationPage implements OnInit {
     document.getElementById("header-registration").style.backgroundColor = "#2196F3";
     document.getElementById("header-registration-label").style.color = "white";
     document.getElementById("header-registration-label").style.backgroundColor = "#2196F3";
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    if () {
+
+    } else {
+      await loading.dismiss();
+    }
   }
 }
