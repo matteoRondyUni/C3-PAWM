a5c97312a4a43b61307ea52144f4404fe4160038
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-05-11 16:12:26 +0200

    Update authentication.service.ts

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { map, tap, switchMap, catchError } from 'rxjs/operators';
-import { BehaviorSubject, from, Observable, Subject, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map, tap, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 import jwt_decode from 'jwt-decode';
 
 import { Plugins } from '@capacitor/core';
-import { ConstantPool } from '@angular/compiler';
-import * as crypto from "crypto-js";
-
 const { Storage } = Plugins;
 
 const TOKEN_KEY = 'my-token';
-
-// var crypto = require("crypto-js");
 
 @Injectable({
   providedIn: 'root'
@@ -35,30 +30,12 @@ export class AuthenticationService {
     }
   }
 
-  async getType() {
-    return (await Storage.get({ key: "TipoUtente" })).value;
+  async getToken() {
+    const toReturn = await Storage.get({ key: TOKEN_KEY });
+    return toReturn;
   }
 
   login(credentials: { email, password }): Observable<any> {
-    // const request = new XMLHttpRequest();
-    // request.open('POST', '/users/login', true);
-    // request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    // request.send(JSON.stringify(credentials));
-    // request.onload = function () {
-    //   if (this.status >= 200 && this.status < 400) {
-    //     const data = JSON.parse(this.response);
-    //     Storage.set({ key: TOKEN_KEY, value: data.accessToken });
-    //     console.log("dati: ", data);
-    //     const dastampre = Storage.get({ key: TOKEN_KEY });
-    //     console.log("storage: ", dastampre);
-    //     Storage.set({ key: "TipoUtente", value: data.tipoUtente });
-    //     return true;
-    //   } else {
-    //     return false;
-    //     // We reached our target server, but it returned an error
-    //   }
-    // }
-
     return this.http.post('/users/login', credentials).pipe(
       map((data: any) => data.accessToken),
       switchMap(token => {
@@ -78,7 +55,6 @@ export class AuthenticationService {
         this.isAuthenticated.next(true);
       })
     )
-
   }
 
   logout(): Promise<void> {
